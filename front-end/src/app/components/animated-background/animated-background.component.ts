1741b71de3fe909bbdbd4d0361811aa568004c77
Pedro Junior <49008209+PedruJr@users.noreply.github.com>
2020-07-10 09:15:53 -0300

    abouts page
    doing

--- front-end/src/app/components/animated-background/animated-background.component.ts
+++ front-end/src/app/components/animated-background/animated-background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ScriptService } from 'src/app/services/script.service';
 
 @Component({
@@ -7,14 +7,17 @@ import { ScriptService } from 'src/app/services/script.service';
   styleUrls: ['./animated-background.component.scss'],
   providers: [ScriptService],
 })
+
 export class AnimatedBackgroundComponent implements OnInit {
 
-  constructor(private service: ScriptService) {
+  @Input() title;
+  @Input() text;
+  
 
+  constructor(private service: ScriptService) {
     service.load('file1', 'file2', 'file3').then(data => {
       console.log('script loaded ', data);
     }).catch(error => console.log(error));
-
    }
 
   ngOnInit() {
